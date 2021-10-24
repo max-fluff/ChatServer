21b8244f0585bae1152b0aef1e8e868c6ff15bf3
Max <borshishe@gmail.com>
2021-10-24 16:06:11 +0300

    init commit

---
+++ ChatServer/Startup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+ 
+namespace ChatServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+ 
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+ 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+ 
+            app.UseRouting();
+ 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
