1391a714fc8e6a9c71111089bcc10d26b4303cd1
Max <borshishe@gmail.com>
2021-10-27 23:23:09 +0300

    timestamp sending

--- ChatServer/Hubs/ChatHub.cs
+++ ChatServer/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace ChatServer
@@ -7,7 +8,11 @@ namespace ChatServer
     {
         public async Task Send(string name, string message)
         {
-            await Clients.All.SendAsync("Send", name, message);
+            var epochTicks = new TimeSpan(new DateTime(1970, 1, 1).Ticks);
+            var unixTicks = new TimeSpan(DateTime.UtcNow.Ticks) - epochTicks;
+            var unixTime = MathF.Floor((float) unixTicks.TotalSeconds);
+            
+            await Clients.All.SendAsync("Send", name, message, unixTime);
         }
     }
 }
