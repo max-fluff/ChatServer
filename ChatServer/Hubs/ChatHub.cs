568ee7a1384c61c9956b85511d40763be077644e
Max <borshishe@gmail.com>
2021-11-21 13:57:18 +0300

    room creation and connection

--- ChatServer/Hubs/ChatHub.cs
+++ ChatServer/Hubs/ChatHub.cs
@@ -6,13 +6,32 @@ namespace ChatServer
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string name, string message)
+        public async Task Send(string groupName, string name, string message)
         {
             var epochTicks = new TimeSpan(new DateTime(1970, 1, 1).Ticks);
             var unixTicks = new TimeSpan(DateTime.UtcNow.Ticks) - epochTicks;
             var unixTime = (int) MathF.Floor((float) unixTicks.TotalSeconds);
 
-            await Clients.All.SendAsync("Send", name, message, unixTime);
+            await Clients.Group(groupName).SendAsync("Send", name, message, unixTime);
+        }
+
+        public async Task RequestNewRoom()
+        {
+            var groupName = Guid.NewGuid().ToString();
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.SendAsync("OnRoomConnect", groupName);
+        }
+
+        public async Task JoinRoom(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.SendAsync("OnRoomConnect", groupName);
+        }
+
+        public async Task LeaveRoom(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Caller.SendAsync("OnLeaveRoom", groupName);
         }
     }
 }
